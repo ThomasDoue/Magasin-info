1cd9e956d5aff672f7cbc9b1c279551f7aa6acac
Swapeur ▪︎ <thomasdoue49100gmail.com>
2020-11-10 13:18:04 +0100

    full commit

---
+++ Class_Client.py
@@ -0,0 +1,5 @@
+class Client:
+	def __init__(self, nom, prenom, choix):
+		self.nom = nom
+		self.prenom = prenom
+		self.choix = choix
---
+++ Class_Setup.py
@@ -0,0 +1,8 @@
+class Setup:
+	def __init__(self, prix, stock, config, marque, GPU, CPU):
+		self.prix = prix
+		self.stock = stock
+		self.config = config
+		self.marque = marque
+		self.GPU = GPU
+		self.CPU = CPU
